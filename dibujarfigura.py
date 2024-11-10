e18bc7ecc2c113cc64675cb66975c394533088dc
unknown <juank27gm@gmail.com>
2024-11-10 18:03:17 -0500

    se corrigio el codigo de dibuarfigura

--- dibujarfigura.py
+++ dibujarfigura.py
@@ -1,11 +1,8 @@
 n = int(input("Ingresa un numero: "))
-for i in range(1, n+1):
-    for j in range(1, n+1):
-        if i == 1 or i == n:
-            print("* ", end="")
+for i in range(0,n):
+    for j in range(0,n):
+        if i==0 or i==n-1 or j==0 or j==n-1:
+            print("*",end=" ")
         else:
-            if j == 1 or j == n:
-                print("* ", end="")
-            else:
-                print("  ", end="")
-    print("")
+            print(" ",end=" ")
+    print()
